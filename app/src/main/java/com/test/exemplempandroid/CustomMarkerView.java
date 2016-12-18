fbd85df3bb2c0bfdafa174c770022f68df082127
Malcom <gtshilombowanticale@adneom.com>
2016-12-18 15:59:56 +0100

    adding custom marker view

---
+++ app/src/main/java/com/test/exemplempandroid/CustomMarkerView.java
@@ -0,0 +1,37 @@
+package com.test.exemplempandroid;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.utils.MPPointF;
+
+/**
+ * Created by gtshilombowanticale on 18-12-16.
+ */
+public class CustomMarkerView extends MarkerView {
+
+    private TextView valueText;
+
+    /**
+     * Constructor. Sets up the MarkerView with a custom layout resource.
+     *
+     * @param context
+     * @param layoutResource the layout resource to use for the MarkerView
+     */
+    public CustomMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+        valueText = (TextView)findViewById(R.id.id_value);
+    }
+
+    // callbacks everytime the MarkerView is redrawn, can be used to update the
+    // content (user-interface)
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        super.refreshContent(e, highlight);
+        valueText.setText(String.format("%d",e.getY()));
+    }
+    
+}
